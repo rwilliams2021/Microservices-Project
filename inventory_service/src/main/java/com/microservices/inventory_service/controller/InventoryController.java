16f5fbf726c13d25e6fa87245bcd381a6c8e8296
r.williams <richyrichw93@gmail.com>
2025-06-27 22:30:07 +0200

    update

---
+++ inventory_service/src/main/java/com/microservices/inventory_service/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.microservices.inventory_service.controller;
+
+import com.microservices.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
